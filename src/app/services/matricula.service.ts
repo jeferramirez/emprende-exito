4162b41f0eeb71ffa33b8f7dd73a406d18b025e0
Jeff <jefer.ramirezjrt@hotmail.com>
2020-11-16 15:55:59 -0500

    create and delete matricula

---
+++ src/app/services/matricula.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatriculaService {
+
+  constructor(private http: HttpClient) { }
+
+
+  getMatriculas(): Observable<any> {
+    return this.http.get(`${environment.URLAPI}/matriculas`);
+  }
+
+
+  createMatricula(matricula): Observable<any> {
+    return this.http.post(`${environment.URLAPI}/matriculas`, matricula);
+  }
+
+  deleteMatricula(idMatricula): Observable<any> {
+    return this.http.delete(`${environment.URLAPI}/matriculas/${idMatricula}`);
+  }
+}
